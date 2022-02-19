d97cf0df59582cb9f8654fa1aba9cd702a8f0f09
DataBaseErased <celsocvs@hotmail.com>
2022-02-19 10:59:08 -0300

    update All

--- comparison.py
+++ comparison.py
@@ -1,78 +1,98 @@
-# Simple pygame program
-#!updatte
-# Import and initialize the pygame library
-from random import randint
+from asyncio import futures
+from pickletools import pybytes
 import pygame
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
+import random
+import cmCamera
 
-pygame.init()
-G = 1
+vec = pygame.Vector2
 FPS = 60
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+WIDTH = 750
+HEIGHT = 600
+
+backgroundSize = vec(10000,10000)
+trajectoriesSurface = pygame.Surface(backgroundSize)
+
+janela = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Universe simulator")
+
+
+
+
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-class bola():
-    def __init__(self, x, y, vx, vy,m):
-        self.r = pygame.Vector2(x,y)
-        self.v = pygame.Vector2(vx,vy)
-        self.a = pygame.Vector2(0,0)
+class obj():
+    def __init__(self,rx,ry,vx,vy,m) :
+        self.r = vec(rx,ry)
+        self.v = vec(vx,vy)
         self.m = m
-
-    def feelForce(self, rOther, mOther):
-        #posição relativa
-        rRel = pygame.Vector2(rOther.x - self.r.x, rOther.y - self.r.y)
-        print(self.a)
-        self.a =  mOther * (1/rRel.length_squared()) *rRel.normalize()*G
-
-    def draw(self):
-        pygame.draw.circle(screen, (0,0,0), (self.r), 10)
-        self.r += self.v*1/FPS
-        self.v += self.a*1/FPS
+        self.traj = []
+    
+    
+    def showSelf(self):
         
+        if(self.m < 100):
+            pygame.draw.circle(janela,(92, 64, 51),self.r , 10)
+        else:
+            pygame.draw.circle(janela,(92, 64, 51),self.r , 30)
+        self.r += self.v /FPS
         
 
 
-b1 = bola(100,100, 50, 60, 400000)
-b2 = bola(400, 300,-30, 30,700000)
+    def grav(self, bodies):
+        for body in bodies:
+            if self != body:
+                rRel = vec(body.r.x - self.r.x, body.r.y - self.r.y)
+                self.v += body.m * (1/rRel.length_squared()) *rRel.normalize() *100
 
-numeroBolas = 3
-bList = []
-# Run until the user asks to quit
-running = True
-while running:
+    def showTraj(self,trajSurf):
+        
+        for i in self.traj:
+           # print(i)
+            pygame.draw.circle(trajSurf,(0,0,100),i,2)
 
+        
 
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    c1 = obj(100,100,-40,-40, 1000)
+    
+    corpos = [c1]
 
+    cam = cmCamera.camMov(janela, corpos, vec(WIDTH/2,HEIGHT/2))
+    
+    def generateSys():
+        for i in range(5):
+            randPos = vec(random.randint(c1.r.x - WIDTH,c1.r.x + WIDTH ) , random.randint(c1.r.y - HEIGHT,-c1.r.y + HEIGHT ) )
+            randVel = vec(random.randint(c1.v.x - 100, 100-c1.v.x), random.randint(c1.v.y - 100, 100-c1.v.y))
+            corpos.append(obj(randPos.x,randPos.y,randVel.x, randVel.y, 50))
+            
+        pass
+    generateSys()
+   
+    while(run):
+        janela.fill((0,0,0))
+        janela.blit(trajectoriesSurface,(0,0))
+        clock.tick(FPS)
+        
+        #desenha o fundo de estrelas na superficie da classe 
+        cam.moveStars()
+        #calculos
+        cam.defineCM()
+        
+     
+        for j in corpos:
+            j.showSelf()
+            j.grav(corpos)
+            j.showTraj(trajectoriesSurface)
+        
+        
+        
+        pygame.display.update()
 
-# Fill the background with white
-    screen.fill((255, 255, 255))
-
-
-    b1.draw()
-    b2.draw()
-
-
-    b1.feelForce(b2.r, b2.m)
-    b2.feelForce(b1.r, b1.m)
-
-
-
-    # Flip the display
-    pygame.display.flip()
-    clock.tick(FPS)
-# Done! Time to quit.
-pygame.quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+        
+main()
